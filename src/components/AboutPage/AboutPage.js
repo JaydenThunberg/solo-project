1eba8cfa7d3fb3fdd29b9a4223ba3980bea740b2
JaydenThunberg <thunberg.jayden@gmail.com>
2020-07-01 15:16:59 -0500

    updated about page

--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -8,9 +8,30 @@ import React from 'react';
 const AboutPage = () => (
   <div>
     <div>
-      <p>
-        This about page is for anyone to read!
+      <p align="center" >
+      This is an app designed around the needs and wants of the vintage and pinup communities.
+      <br/> 
+      Many of my friends have said they have struggled to find events, clothing, and friends 
+      who share similar interests related to these communities. 
+      My goal was to design an app that does just that. There will be an event page that allows 
+      users to find events and stores page that shows stores that carry the different styles 
+      of clothing. In the future, there will be a feature that will show users who have registered 
+      with the app and allow them to connect with others. Eventually there will be a search function 
+      to allow users to find others based on similarities such as, preferred category, era, or location. 
+      They will then be able to connect through whichever social media platform the users decide on.
       </p>
+      <ul>
+        <span>Technologies Used:</span>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>React</li>
+        <li>Redux</li>
+        <li>Redux-Saga</li>
+        <li>Express</li>
+        <li>Node.js</li>
+        <li>PostrgreSQL</li>
+        <li>Responsive Web Design(Flexbox, Flexbox-Grid)</li>
+      </ul>
     </div>
   </div>
 );
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -7,7 +7,7 @@ import './Nav.css';
 const Nav = (props) => (
   <div className="nav">
     <Link to="/home">
-      <h2 className="nav-title">Prime Solo Project</h2>
+      <h2 className="nav-title"><em>Let's Go, Retro!</em></h2>
     </Link>
     <div className="nav-right">
       <Link className="nav-link" to="/home">
