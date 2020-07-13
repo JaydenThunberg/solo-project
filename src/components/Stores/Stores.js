04e75f4f28575f996055e4dbf96cb99cfc0da26e
JaydenThunberg <thunberg.jayden@gmail.com>
2020-07-12 22:15:14 -0500

    added mui table to stores page w/ map reduState

--- src/components/Stores/Stores.js
+++ src/components/Stores/Stores.js
@@ -3,6 +3,9 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import StoreItem from '../StoreItem/StoreItem';
 
+//material-ui for temporary table
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@material-ui/core';
+
 class Stores extends Component {
   // Renders the entire Stores on the DOM
   componentDidMount() {
@@ -12,10 +15,51 @@ class Stores extends Component {
     console.log('props in stores are:', this.props)
   }//end componentDidMount
 
-  render() {
-    return (
-      <div className="stores">
-        <h1>Stores</h1>
+  //temporary table
+// createData = (name, calories, fat, carbs, protein) => {
+//   return { name, calories, fat, carbs, protein };
+// }
+
+// const rows = [
+//   createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+//   createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+//   createData('Eclair', 262, 16.0, 24, 6.0),
+//   createData('Cupcake', 305, 3.7, 67, 4.3),
+//   createData('Gingerbread', 356, 16.0, 49, 3.9),
+// ];
+render() {
+  // const name = row.store_name;
+  // const description = row.description;
+  // const location = row.location;
+  // const link = <span><a href={row.link}>{row.store_name}</a></span>
+  return (
+    <div className="stores">
+      <TableContainer>
+        <Table aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Store Name</TableCell>
+              <TableCell align="left">Description</TableCell>
+              <TableCell align="left">Location</TableCell>
+              <TableCell align="left">Link</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {this.props.reduxState.stores.map((item) => (
+              <TableRow key={item.store_name}>
+                <TableCell component="th" scope="row">
+                {item.store_name}
+                </TableCell>
+                {/* <TableCell align="right">{name}</TableCell> */}
+                <TableCell align="left">{item.description}</TableCell>
+                <TableCell align="left">{item.location}</TableCell>
+                <TableCell align="left">{<span><a href={item.link}>{item.store_name}</a></span>}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      {/* <h1>Stores</h1>
         <table>
           <thead>
             <tr>
@@ -30,10 +74,10 @@ class Stores extends Component {
           return (
             <StoreItem key={item.store_name} item={item} history={this.props.history} />
           )
-        })}
-      </div>
-    );//end return
-  }//end render
+        })} */}
+    </div>
+  );//end return
+}//end render
 }// end class
 
 const putReduxStateOnProps = (reduxState) => ({ reduxState });
